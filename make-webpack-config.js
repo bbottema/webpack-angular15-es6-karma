8b719fe35464842000ed1c9152f046da1edd00e3
Bogdan Savluk <savluk.bogdan@gmail.com>
2016-07-22 02:42:59 +0300

    move config creation to separate function in order to allow passing options for different cases

---
+++ make-webpack-config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function (options) {
+	return {
+		debug: true,
+		entry: {
+			app: ['./app/index.js'],
+			vendor: ['./app/vendor.js']
+		},
+		output: {
+			path: path.join(__dirname, 'dist'),
+			filename: 'js/[name].js'
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './app/index.html',
+				inject: 'body',
+				minify: false
+			}),
+			// new webpack.optimize.CommonsChunkPlugin('vendor', 'js/vendor.js'),
+			new webpack.SourceMapDevToolPlugin({
+				filename: '[file].map',
+				exclude: /vendor/
+			})
+		],
+		resolve: {
+			extensions: ['', '.js'],
+			alias: {
+				app: path.join(__dirname, 'app')
+			}
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					include: [
+						path.join(__dirname, 'app'),
+						path.join(__dirname, 'test')
+					],
+					exclude: path.join(__dirname, 'node_modules')
+				},
+				{
+					test: /\.html$/,
+					loader: 'html-loader'
+				}
+			]
+		},
+		resolveLoader: {
+			root: path.join(__dirname, 'node_modules')
+		}
+	};
+}
